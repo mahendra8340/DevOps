b5589a033277ee99095839d4cfc7f45a7fa059f3
mahendra8340 <mahendra834@gmail.com>
2025-01-06 19:01:54 -0500

    Puthon till day 4

---
+++ python/Day-04/calc.py
@@ -0,0 +1,19 @@
+a =5
+b=6
+
+def add(a,b ):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+print(f"addition of {a} and {b} is "+ str(add(a,b)))
+print(f"Subtraction of {a} and {b} is "+str(sub(a,b)))
+print(f"Multiplciation of {a} and {b} is "+str(mul(a,b)))
+print(f"division of {a} and {b} is "+str(div(a,b)))
