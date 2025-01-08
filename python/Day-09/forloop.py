c4df7bcdefc134a7aaa76eaea66aa73a47dc066c
mahendra8340 <mahendra834@gmail.com>
2025-01-08 16:29:50 -0500

    Forget to add day10

---
+++ python/Day-09/forloop.py
@@ -0,0 +1,10 @@
+my_list=['yellow','red','blue','green']
+for item in my_list:
+    print(item.upper())
+
+
+while True:
+    print('Enter your name:')
+    name=input()
+    if name=='break':
+        break
