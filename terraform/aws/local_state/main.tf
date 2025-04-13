2589e46db4073872e89f4c2a758dd14c10426572
mahendra8340 <mahendra834@gmail.com>
2025-04-13 19:25:49 -0400

    Updated terraform files

--- terraform/aws/local_state/main.tf
+++ terraform/aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-00a929b66ed6e0de6"
   instance_type = "t2.micro"
 
   tags = {
